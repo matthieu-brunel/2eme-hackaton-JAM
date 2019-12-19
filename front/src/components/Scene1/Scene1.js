b243ac5ea31e40350bcca223300c46f56dfdfd3f
JeremyLP <j.lepuill232@laposte.net>
2019-12-19 16:14:44 +0100

    Push de l'appel à l'api et de l'interaction avec le personnage de la scène 1

--- front/src/components/Scene1/Scene1.js
+++ front/src/components/Scene1/Scene1.js
@@ -2,10 +2,12 @@ import React from 'react';
 import "./Scene1.css"
 import CompScene1 from "./compScene1"
 
+
 function Scene1() {
   return (
     <div className="backScene1">
-      <CompScene1/>
+    <CompScene1/>
+    
       
 
     </div>
