f92c70fb99b30cf05ba89dc32a93a7ac2198fb78
Alexandra Brazy <morthis1014@gmail.com>
2019-12-19 20:47:30 +0100

    implant des personnages des scene 4 & 5

--- back/routes/conf.js
+++ back/routes/conf.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    password: 'maclaud6',
+    password: 'morthis1014',
     database: 'hackathon',
     timezone : 'local', 
     'dateStrings':[
--- front/src/components/Scene4/compScene4.js
+++ front/src/components/Scene4/compScene4.js
@@ -2,21 +2,79 @@ import React from 'react';
 import "./compScene4.css";
 import "./../Gridd.css";
 
-class CompScene4 extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
 
-    render() {
-        return (
-            <div >
-                <div className="Scene4">
-            <a href="/scene5" class="Scene4Button">Commencer</a>
-                </div> 
-            </div>
-        )
-    }
-}
+    
+    
+    class CompScene4 extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                isToggleOn: true,
+                dialogue: ""
+            };
+        }
+    
+        componentDidMount() {
+            fetch('http://localhost:5000/api/charInfo/text')
+            .then((res) => {
+                console.log("res: ",res);
+                return res.json()
+                }
+            )
+            .then(data => {
+                console.log("data: ",data);
+                this.setState({dialogue: data});
+            })
+            .catch(err => {
+                console.log("error: ", err)
+            });
+        }
+    
+        getMyDialogue = () => {
+    
+        
+    
+        }
+        render() {
+            return (
+                <div >
+                    <div className="Scene4">
+                        <a href="/scene5" class="Scene1Button">Suivant</a>
+                        <div>
+                            {!this.state.isToggleOn && <div className={this.state.isToggleOn ? "dialogue" : ""}  >
+                                <p className="bubble">{this.state.dialogue[0].text}  </p>
+    
+                            </div>}
+    
+                            <img onClick={event => {
+                                this.state.isToggleOn ? this.setState({ isToggleOn: false }) : this.setState({ isToggleOn: true })
+                            }} src="../../asset/matcoco.png" alt="Un personnage" />
 
-export default CompScene4;
+
+                            <img onClick={event => {
+                                this.state.isToggleOn ? this.setState({ isToggleOn: false }) : this.setState({ isToggleOn: true })
+                            }} src="../../asset/louis.png" alt="Un personnage" />
+
+
+                            <img onClick={event => {
+                                this.state.isToggleOn ? this.setState({ isToggleOn: false }) : this.setState({ isToggleOn: true })
+                            }} src="../../asset/armand.png" alt="Un personnage" /> 
+
+                            <img onClick={event => {
+                                this.state.isToggleOn ? this.setState({ isToggleOn: false }) : this.setState({ isToggleOn: true })
+                            }} src="../../asset/jonathan.jpg.png" alt="Un personnage" /> 
+
+                            <img onClick={event => {
+                                this.state.isToggleOn ? this.setState({ isToggleOn: false }) : this.setState({ isToggleOn: true })
+                            }} src="../../asset/baptiste_a.png" alt="Un personnage" /> 
+    
+                        </div>
+    
+                    </div>
+                </div>
+            )
+        }
+    }
+    
+    export default CompScene4;
+
